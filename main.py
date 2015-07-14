b3c6514c69368ce24cf11638d54ebe784445bb06
Zachary Jones <zacharytamas@gmail.com>
2015-07-14 18:39:24 -0400

    Add initial tempting boilerplate

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python
 
+import jinja2
+import os
 import webapp2
 
 from importers import parse_path
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.join(os.path.dirname(__file__), "templates")),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
 
 
 class MainHandler(webapp2.RequestHandler):
