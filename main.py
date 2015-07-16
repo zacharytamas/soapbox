cfe06f61ba5191b4fb789c042fa5456e9b87fefe
Zachary Jones <zacharytamas@gmail.com>
2015-07-15 20:21:01 -0400

    Final change for this session. Cleanup code style.

--- main.py
+++ main.py
@@ -3,33 +3,32 @@
 import jinja2
 import os
 import webapp2
+from os.path import join
 
 from importers import parse_path
 from blog.models import Post
 
 JINJA_ENVIRONMENT = jinja2.Environment(
-    loader=jinja2.FileSystemLoader(os.path.join(os.path.dirname(__file__), "templates")),
-    extensions=['jinja2.ext.autoescape'],
-    autoescape=True)
+  loader=jinja2.FileSystemLoader(join(os.path.dirname(__file__), "templates")),
+  extensions=['jinja2.ext.autoescape'],
+  autoescape=True)
 
 
-class MainHandler(webapp2.RequestHandler):
+class HomepageHandler(webapp2.RequestHandler):
   def get(self):
     parse_path("posts/2014-01-20-how-i-work-coding/how-i-work-coding.md").process()
     parse_path("posts/2014-01-02-es6-arrow-functions/es6-arrow-functions.md").process()
     parse_path("posts/2014-01-12-annual-goals/annual-goals.md").process()
 
-    posts = Post.getLatest()
-
     template = JINJA_ENVIRONMENT.get_template('index.html')
 
     context = {
-      "posts": posts
+      "posts": Post.getLatest()
     }
 
     self.response.write(template.render(context))
 
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+  ('/', HomepageHandler)
 ], debug=True)
