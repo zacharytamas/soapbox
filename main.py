5f028d83a0bf732ed981c426d264e4126ca9614c
Zachary Jones <zacharytamas@gmail.com>
2015-07-16 19:05:39 -0400

    Extract the updating to a separate UpdateHandler.
    This will eventually be the handler that handles getting a POST from GitHub about newly pushed commits.

--- main.py
+++ main.py
@@ -16,10 +16,6 @@ JINJA_ENVIRONMENT = jinja2.Environment(
 
 class HomepageHandler(webapp2.RequestHandler):
   def get(self):
-    parse_path("posts/2014-01-20-how-i-work-coding/how-i-work-coding.md").process()
-    parse_path("posts/2014-01-02-es6-arrow-functions/es6-arrow-functions.md").process()
-    parse_path("posts/2014-01-12-annual-goals/annual-goals.md").process()
-
     template = JINJA_ENVIRONMENT.get_template('index.html')
 
     context = {
@@ -29,6 +25,14 @@ class HomepageHandler(webapp2.RequestHandler):
     self.response.write(template.render(context))
 
 
+class UpdateHandler(webapp2.RequestHandler):
+  def get(self):
+    parse_path("posts/2014-01-20-how-i-work-coding/how-i-work-coding.md").process()
+    parse_path("posts/2014-01-02-es6-arrow-functions/es6-arrow-functions.md").process()
+    parse_path("posts/2014-01-12-annual-goals/annual-goals.md").process()
+
+
 app = webapp2.WSGIApplication([
-  ('/', HomepageHandler)
+  ('/', HomepageHandler),
+  ('/_hooks/update', UpdateHandler)
 ], debug=True)
