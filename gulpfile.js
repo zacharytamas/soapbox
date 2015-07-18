beff7912957e12f3a6f5034a5d80cd4c4113666a
Zachary Jones <zacharytamas@gmail.com>
2015-07-18 19:50:28 -0400

    Clean up on gulpfile. Nitpicks, really.
    Not doing much work on this tonight but I am thinking about what the front-end development cycle will be like.

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,15 @@
 'use strict';
 
 // Include Gulp & Tools We'll Use
-var gulp = require('gulp');
-var $ = require('gulp-load-plugins')();
-var merge = require('merge-stream');
+var gulp = require('gulp'),
+    $ = require('gulp-load-plugins')(),
+    merge = require('merge-stream');
 
 function distPath(path) {
   return 'static/dist/' + path;
 }
 
 gulp.task('copy', function() {
-
   var bower = gulp.src([
     'bower_components/**/*'
   ]).pipe(gulp.dest(distPath('bower_components')));
@@ -25,7 +24,6 @@ gulp.task('copy', function() {
 
   return merge(bower, elements, vulcanized)
     .pipe($.size({title: 'copy'}));
-
 });
 
 // Vulcanize imports
